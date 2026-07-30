8c3a54567ff1f51536945c57d87509a37f5dcb3a
Chijioke Joseph <chijiokejoseph2022@gmail.com>
2026-07-30 07:57:11 +0100

    Fix issue #164: update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Toaster } from 'sonner';
 import EnvCheck from '@/components/EnvCheck';
 import SWRegister from '@/components/SWRegister';
 import Script from 'next/script';
+import AccessibilityAnnouncer from '@/components/AccessibilityAnnouncer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -43,6 +44,7 @@ export default function RootLayout({
         <Provider>
           <SWRegister />
           <EnvCheck />
+          <AccessibilityAnnouncer />
           <a
             href="#main-content"
             className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-9999 focus:bg-white focus:text-black focus:px-4 focus:py-2 focus:rounded focus:font-semibold"
